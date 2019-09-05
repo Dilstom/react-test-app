be4a196c6e9543ab1b1809028defafcffe9f8034
Dil Sombayeva <dilyasom@gmail.com>
2019-09-05 09:06:47 -0700

    onClick event

--- counter/src/Profile.js
+++ counter/src/Profile.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 
 class Profile extends Component {
+ //  handleSelect = () => this.props.handleSelect(this.props.id);
  render() {
+  console.log(this.props);
   return (
    <div>
     <img src="https://via.placeholder.com/250" alt="pic" />
     <h4>{this.props.name}</h4>
     <p>{this.props.position}</p>
+    <button onClick={() => this.props.handleSelect(this.props.id)}>
+     {/* <button onClick={this.handleSelect}> */}
+     Set {this.props.name} as Employee Of The Month
+    </button>
    </div>
   );
  }
